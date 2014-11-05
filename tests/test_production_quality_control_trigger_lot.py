3f9a437e72c3357278664102d37b7a458429911a
Sergi Almacellas Abellana <sergi@koolpi.com>
2014-11-05 10:17:58 +0100

    Migrate to version 3.4

--- tests/test_production_quality_control_trigger_lot.py
+++ tests/test_production_quality_control_trigger_lot.py
@@ -4,13 +4,11 @@
 import unittest
 import doctest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import doctest_dropdb
+from trytond.tests.test_tryton import doctest_setup, doctest_teardown
 
 
 class TestCase(unittest.TestCase):
-    '''
-    Test module.
-    '''
+    'Test module'
 
     def setUp(self):
         trytond.tests.test_tryton.install_module(
@@ -22,6 +20,6 @@ def suite():
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(TestCase))
     suite.addTests(doctest.DocFileSuite(
             'scenario_production_quality_control_trigger_lot.rst',
-            setUp=doctest_dropdb, tearDown=doctest_dropdb, encoding='utf-8',
+            setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
             optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     return suite
