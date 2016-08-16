f9a0e068a17c54ad67bdff68aaeb4e05468dbe6f
Jes?s Mart?n Jim?nez <jesus@nan-tic.com>
2016-08-16 10:27:10 +0200

    Upgrade test

--- tests/test_production_quality_control_trigger_lot.py
+++ tests/test_production_quality_control_trigger_lot.py
@@ -4,15 +4,14 @@
 import unittest
 import doctest
 import trytond.tests.test_tryton
+from trytond.tests.test_tryton import ModuleTestCase
 from trytond.tests.test_tryton import doctest_setup, doctest_teardown
+from trytond.tests.test_tryton import doctest_checker
 
 
-class TestCase(unittest.TestCase):
+class TestCase(ModuleTestCase):
     'Test module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module(
-            'production_quality_control_trigger_lot')
+    module = 'production_quality_control_trigger_lot'
 
 
 def suite():
@@ -21,5 +20,6 @@ def suite():
     suite.addTests(doctest.DocFileSuite(
             'scenario_production_quality_control_trigger_lot.rst',
             setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
+            checker=doctest_checker,
             optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     return suite
